f6c23b3db25570908d0942e7abc3cd26f58fac94
ilo80 <ilo80.pro@gmail.com>
2024-12-27 23:16:00 +0100

    feat: implement ping command in the bot

--- src/bot.ts
+++ src/bot.ts
@@ -1,12 +1,31 @@
 import { Client, Events, GatewayIntentBits, REST, Routes } from "discord.js";
 import dotenv from "dotenv";
 
+// Import commands
+import { ping } from "./commands/ping";
+
 dotenv.config(); // Load .env file
 
 export const client = new Client( { intents: GatewayIntentBits.Guilds } ); // Create a new client with the Guilds intent
 
 const rest = new REST().setToken( process.env.DISCORD_TOKEN as string ); // Create a new REST client and set the token
 
+const commands = [ ping ]; // List of commands
+
 client.once(Events.ClientReady, async () => {
     console.log("Bot is ready!");
+
+    await rest.put( Routes.applicationGuildCommands( client.user?.id as string, process.env.GUILD_ID as string ), { body: commands } ); // Register commands
+
+    console.log("Commands registered!");
+});
+
+client.on(Events.InteractionCreate, async (interaction) => {
+    if (!interaction.isCommand()) return;
+
+    const command = commands.find( command => command.name === interaction.commandName ); // Find the command
+
+    if (command) {
+        command.execute( interaction );
+    }
 });
