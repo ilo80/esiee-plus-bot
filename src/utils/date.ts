147afca26170f97ff7f165545f4dc30998ed5d01
ilo80 <ilo80.pro@gmail.com>
2024-12-28 19:08:52 +0100

    feat(utils): add isValideDate function to validates dates in DD/MM/YYYY format

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -18,3 +18,21 @@ export const doTimeRangeOverlap = (start1: string, end1: string, start2: string,
 
     return start1Minutes < end2Minutes && start2Minutes < end1Minutes;
 };
+
+export const isValideDate = (date: string) => {
+    const parts = date.split('/').map(Number);
+
+    if (parts.length !== 3) {
+        return false;
+    }
+
+    parts[1] -= 1; // Month is 0-based
+
+    if (isNaN(parts[0]) || isNaN(parts[1]) || isNaN(parts[2])) { // Check if parts are numbers
+        return false;
+    }
+
+    const dateObj = new Date(parts[2], parts[1], parts[0]);
+
+    return dateObj.getDate() === parts[0] && dateObj.getMonth() === parts[1] && dateObj.getFullYear() === parts[2];
+}
