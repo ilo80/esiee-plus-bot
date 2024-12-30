d5ff1c80d5f7cdb4f565e642f93370c62bd1031a
ilo80 <ilo80.pro@gmail.com>
2024-12-30 16:10:59 +0100

    refactor(time): reorganize function order for improved readability

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -3,6 +3,30 @@ export type Time = {
     minutes: number;
 };
 
+export const isValidTimeString = (time: string) => {
+    const parts = time.split(':').map(Number);
+
+    if (parts.length !== 2) {
+        return false;
+    }
+
+    if (isNaN(parts[0]) || isNaN(parts[1])) {
+        return false;
+    }
+
+    return parts[0] >= 0 && parts[0] <= 23 && parts[1] >= 0 && parts[1] <= 59;
+};
+
+export const convertTimeFormat = (time: string) => {
+    if (!isValidTimeString(time)) {
+        throw new Error('Invalid time format');
+    }
+
+    const [hours, minutes] = time.split(':').map(Number);
+
+    return { hours, minutes } as Time;
+};
+
 export const doTimeRangeOverlap = (start1: string, end1: string, start2: string, end2: string) => {
     const start1Minutes = timeToMinutes(start1);
     const end1Minutes = timeToMinutes(end1);
@@ -18,19 +42,6 @@ export const timeToMinutes = (time: string) => {
     return hours * 60 + minutes;
 }
 
-export const isValidTimeString = (time: string) => {
-    const parts = time.split(':').map(Number);
-
-    if (parts.length !== 2) {
-        return false;
-    }
-
-    if (isNaN(parts[0]) || isNaN(parts[1])) {
-        return false;
-    }
-
-    return parts[0] >= 0 && parts[0] <= 23 && parts[1] >= 0 && parts[1] <= 59;
-};
 
 export const add1Hour = (hour: string) => {
     const [h, m] = hour.split(":").map(Number);
@@ -42,12 +53,3 @@ export const add1Hour = (hour: string) => {
     return newHourDate.toLocaleTimeString("fr-FR", { hour: "2-digit", minute: "2-digit" });
 };
 
-export const convertTimeFormat = (time: string) => {
-    if (!isValidTimeString(time)) {
-        throw new Error('Invalid time format');
-    }
-
-    const [hours, minutes] = time.split(':').map(Number);
-
-    return { hours, minutes } as Time;
-}
