0c329a7a8082a88b30555a4533e8bcc3c9bb0e1c
ilo80 <ilo80.pro@gmail.com>
2024-12-31 11:01:02 +0100

    refactor: Refactor getAvailableClassroom function tu use helper function

--- src/utils/ade.ts
+++ src/utils/ade.ts
@@ -1,6 +1,31 @@
-import { ADEPlanningAPI } from "ade-planning-api";
+import { ADEPlanningAPI, Resources } from "ade-planning-api";
 import { sleep } from "./sleep";
 import { convertTimeFormat, doTimeRangeOverlap, Time } from "./time";
+import { Resource } from "ade-planning-api/dist/models/timetable"; // Import the Resource type from the ade-planning-api package
+
+export const filterClassrooms = (resources: Resources) => {
+    return resources.filter((resource) =>
+        resource.category === "classroom" && // Filter only classrooms
+        /^[0-9]+$/.test(resource.name) && // Filter only classrooms with a number
+        !resource.path.toLowerCase().includes("labos") && // Filter out labs
+        !resource.path.toLowerCase().includes("examens") && // Filter out exams
+        !resource.name.startsWith("6") && // Filter out classrooms of the 6th epis
+        resource.name !== "0351" && // Filter out the 0351 classroom
+        resource.name !== "0244" // Filter out the 0244 classroom
+    );
+};
+
+export const checkClassroomAvailability = async (api: ADEPlanningAPI, classroomResource: Resource, date: string, startHour: Time, endHour: Time) => {
+    const events = await api.getEvents({ resources: classroomResource.id, date: date, detail: 3 }); // Get all events of the classroom in the specified date
+
+    for (const event of events) {
+        if (doTimeRangeOverlap(startHour, endHour, convertTimeFormat(event.startHour), convertTimeFormat(event.endHour))) { // Check if the classroom is available
+            return false;
+        }
+    }
+
+    return true;
+};
 
 export const getAvailableClassroom = async (date: string, startHour: Time, endHour: Time) => {
     const api = new ADEPlanningAPI(process.env.ADE_LINK as string);
@@ -12,30 +37,12 @@ export const getAvailableClassroom = async (date: string, startHour: Time, endHo
 
     const resources = await api.getResources({ detail: 3 }); // Get all resources
 
-    const classroom = resources.filter((resource) => 
-        resource.category === "classroom" && // Filter only classrooms
-        /^[0-9]+$/.test(resource.name) && // Filter only classrooms with a number
-        !resource.path.toLowerCase().includes("labos") && // Filter out labs
-        !resource.path.toLowerCase().includes("examens") && // Filter out exams
-        !resource.name.startsWith("6") && // Filter out classrooms of the 6th epis
-        resource.name !== "0351" && // Filter out the 0351 classroom
-        resource.name !== "0244" // Filter out the 0244 classroom
-    );
+    const classroom = filterClassrooms(resources); // Filter classrooms
 
     const availableClassroom = [] as string[];
 
     for (const classroomResource of classroom) {
-        const events = await api.getEvents({ resources: classroomResource.id, date: date, detail: 3 }); // Get all events of the classroom in the specified date
-
-        let isAvailable = true;
-
-        for (const event of events) {
-            if (doTimeRangeOverlap(startHour, endHour, convertTimeFormat(event.startHour), convertTimeFormat(event.endHour))) { // Check if the classroom is available
-                
-                isAvailable = false;
-                break;
-            }
-        }
+        const isAvailable = await checkClassroomAvailability(api, classroomResource, date, startHour, endHour); // Check if the classroom is available
 
         if (isAvailable) {
             availableClassroom.push(classroomResource.name);
